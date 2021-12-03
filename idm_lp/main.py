0607ce946ae804c36a61d5497a5480b29331769f
mistertwink <88252604+mistertwink@users.noreply.github.com>
2021-12-03 14:50:46 +0300

    Update main.py

--- idm_lp/main.py
+++ idm_lp/main.py
@@ -39,7 +39,7 @@ parser.add_argument(
     '--base_domain',
     type=str,
     dest="base_domain",
-    default="https://irisduty.ru",
+    default="https://risingduty.ru",
     help='Базовый домен'
 )
 
